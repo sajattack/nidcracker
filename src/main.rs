d1ebbddb085fe7b65aeb1798faa93a8a507145f4
Paul Sajna <hello@paulsajna.com>
2021-04-02 11:59:59 -0700

    fix handling of line endings

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,16 @@ use itertools::Itertools;
 
 fn main() {
     //let module_names_string = read_to_string("module-names.txt").unwrap();
-    //let module_names_split = module_names_string.split('\n');
-    //let module_names: Vec<&str> = module_names_split.collect();
+    //let module_names: Vec<&str> = module_names_string.lines().collect();
 
     let module_name = "sceSysreg";
 
     let wordlist_string = read_to_string("wordlist.txt").unwrap();
-    let wordlist_split = wordlist_string.split('\n');
-    let wordlist: Vec<&str> = wordlist_split.collect();
+    let wordlist: Vec<&str> = wordlist_string.lines().collect();
 
     let unknown_nids_string = read_to_string("unknown_nids.txt").unwrap();
-    let unk_nids_split = unknown_nids_string.split('\n');
-    let unk_nids: Vec<u32> = unk_nids_split.map(|s| u32::from_str_radix(s, 16).unwrap()).collect();
+    let unk_nids_iter = unknown_nids_string.lines();
+    let unk_nids: Vec<u32> = unk_nids_iter.map(|s| u32::from_str_radix(s, 16).unwrap()).collect();
 
     wordlist.par_iter().for_each(|word| {
         let test_string = module_name.to_string() + word;
